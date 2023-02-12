ae956458882310c92da2d883a4475110a4b142cd
FatimaP11 <fatima.19070051@itsmotul.edu.mx>
2023-02-11 23:07:02 -0600

    paso4

---
+++ tutorial4/data.converter.js
@@ -0,0 +1,11 @@
+export const dateConverter = (locale) => {
+    return {
+      toAttribute: (date) => {
+        return date.toLocaleDateString(locale);
+      },
+      fromAttribute: (value) => {
+        return new Date(value);
+      },
+    };
+  };
+  
